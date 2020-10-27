27b2bf651e197bb45fffbd17be242606f628b640
snorman <snorman35@gmail.com>
2020-10-27 13:12:06 -0500

    Modify to work in wsl2

--- setup.bash
+++ setup.bash
@@ -3,10 +3,10 @@
 # This will setup all the dependencies for the delivery robot
 
 # setup the keys for librealsense
-#sudo apt-key adv --keyserver keys.gnupg.net --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE
-#sudo add-apt-repository "deb http://realsense-hw-public.s3.amazonaws.com/Debian/apt-repo bionic main" -u
+sudo apt-key adv --keyserver keys.gnupg.net --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE
+sudo add-apt-repository "deb http://realsense-hw-public.s3.amazonaws.com/Debian/apt-repo bionic main" -u
 
-#sudo apt update
+sudo apt update
 
 # install the realsense packages
 sudo apt-get install librealsense2-dkms -y
